fa2f4b91d9a41c6f4ac0de76ea1e43011431fe5c
Javier Lete <javier@eskura21.com>
2023-06-08 19:25:52 +0200

    Maqueta estática del componente anuncios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AnunciosComponent } from './anuncios/anuncios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnunciosComponent
   ],
   imports: [
     BrowserModule
