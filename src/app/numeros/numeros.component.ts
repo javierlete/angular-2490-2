f2535ddd739ef9bb60bf9727a3973bfecbe317c5
Javier Lete <javier@eskura21.com>
2023-06-13 20:23:42 +0200

    Eventos de nuevo en números

--- src/app/numeros/numeros.component.ts
+++ src/app/numeros/numeros.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-numeros',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./numeros.component.css']
 })
 export class NumerosComponent {
-
+  @Output() nuevoNumero = new EventEmitter<Digito>();
 }
+
+export type Digito = '0'|'1'|'2'|'3'|'4'|'5'|'6'|'7'|'8'|'9'|',';
