88da512ed5c6f0a6948feb80169c53dbd185521e
Javier Lete <javier@eskura21.com>
2023-06-13 18:57:46 +0200

    Output binding para calculadora

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-calculadora',
@@ -8,22 +8,28 @@ import { Component } from '@angular/core';
 export class CalculadoraComponent {
   pantalla: string = '0';
 
+  @Output() pantallaChange = new EventEmitter<string>();
+  
   private n1!: number;
   private op!: string;
   private n2!: number;
 
   onAC() {
     this.pantalla = '0';
+    this.pantallaChange.emit(this.pantalla);
   }
   onCambiaSigno() {
     this.pantalla = String((-1 * Number(this.pantalla)));
+    this.pantallaChange.emit(this.pantalla);
   }
   onPorcentaje() {
     this.pantalla = String(Number(this.pantalla) / 100);
+    this.pantallaChange.emit(this.pantalla);
   }
 
   onNumero(numero: string) {
     this.pantalla += numero;
+    this.pantallaChange.emit(this.pantalla);
   }
 
   operacion(op: string) {
@@ -54,8 +60,8 @@ export class CalculadoraComponent {
         break;
       default:
         break;
-
-    }
+      }
+      this.pantallaChange.emit(this.pantalla);
   }
   private calcular(): any {
     switch(this.op) {
