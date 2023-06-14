ae7633a7e423ce42926d98df767b0f323c5b3e46
Javier Lete <javier@eskura21.com>
2023-06-14 19:15:01 +0200

    Aritmético funcional

--- src/app/aritmeticas/aritmeticas.component.ts
+++ src/app/aritmeticas/aritmeticas.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-aritmeticas',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./aritmeticas.component.css']
 })
 export class AritmeticasComponent {
-
+  @Output() operacionPulsada = new EventEmitter<string>();
 }
