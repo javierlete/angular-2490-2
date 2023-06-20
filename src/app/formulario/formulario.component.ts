efd0ea002b1ed58052b7a0305d2e85469929a623
Javier Lete <javier@eskura21.com>
2023-06-20 19:30:07 +0200

    Formulario y listado funcionales

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../cliente';
+import { ClienteService } from '../cliente.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css']
 })
-export class FormularioComponent {
+export class FormularioComponent implements OnInit {
+  cliente: Cliente = {} as Cliente;
 
+  constructor(private clienteService: ClienteService, private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit(): void {
+    const id: number = Number(this.route.snapshot.paramMap.get('id'));
+    
+    if (id) {
+      this.clienteService.obtenerPorId(id).subscribe(cliente => this.cliente = cliente);
+    }
+  }
+  guardar() {
+    if (this.cliente.id) {
+      this.clienteService.modificar(this.cliente).subscribe(
+        () => this.router.navigate(['/listado'])
+      );
+    } else {
+      this.clienteService.insertar(this.cliente).subscribe(
+        () => this.router.navigate(['/listado'])
+      );
+    }
+  }
 }
--- src/app/listado/listado.component.ts
+++ src/app/listado/listado.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ClienteService } from '../cliente.service';
+import { Cliente } from '../cliente';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
   styleUrls: ['./listado.component.css']
 })
-export class ListadoComponent {
+export class ListadoComponent implements OnInit {
+  clientes: Cliente[] = [];
+  
+  constructor(private clienteService: ClienteService) {}
+  ngOnInit(): void {
+    this.clienteService.obtenerTodos().subscribe(clientes => this.clientes = clientes);
+  }
+
+  borrar(id: number) {
+    this.clienteService.borrar(id).subscribe(() => this.clientes = this.clientes.filter(cliente => cliente.id !== id));
+  }
 
 }
